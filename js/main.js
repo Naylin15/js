dd6437d13fabba07572e86e5c814e230d574f82d
Naylin Medina <mejiasnam@gmail.com>
2020-04-14 07:31:57 -0400

    refactor: display of notes
    Signed-off-by: Naylin Medina <mejiasnam@gmail.com>

--- js/main.js
+++ js/main.js
@@ -13,40 +13,64 @@ function addNote(e) {
         description: noteDescription
     })
     document.getElementById('form-add').reset();
-    displayNotes();
+    checkNote();
 
+}
+
+function checkNote() {
+    let check = document.getElementById('noteContainer');
+
+    
+    if (check) {
+        check.remove();
+        displayNotes();
+    } else {
+        displayNotes();
+    }
 }
 
 function displayNotes() {
 
-    let i = notes.length - 1;
-
-    let noteContainer = document.createElement("article");
-    noteContainer.setAttribute("class", "note");
-    noteContainer.setAttribute("id", i)
+    let noteContainer = document.createElement("div");
+    noteContainer.setAttribute("class", "noteContainer");
+    noteContainer.setAttribute("id", "noteContainer")
     document.getElementById('notes').appendChild(noteContainer);
 
-    let titleTag = document.createElement("h2");
-    let titleContent = document.createTextNode(notes[i].title);
-    titleTag.appendChild(titleContent);
-        
-    let descriptionTag = document.createElement("p");
-    let descriptionContent = document.createTextNode(notes[i].description);
-    descriptionTag.appendChild(descriptionContent);
+    for (i=0; i < notes.length; i++){
 
-    let removeButton = document.createElement("button");
-    removeButton.setAttribute("class", "removeNote icon-bin");
-    removeButton.setAttribute("onclick", "removeNote(this.parentElement.id)");
-        
-    let editButton = document.createElement("button");
-    editButton.setAttribute("class", "editButton icon-edit");
-    editButton.setAttribute("onclick", "editNote(this.parentElement.id)");
+        let noteArticle = document.createElement("article");
+        noteArticle.setAttribute("class", "note");
+        noteArticle.setAttribute("id", i)
+        document.getElementById('noteContainer').appendChild(noteArticle);
     
-    document.getElementById(i).appendChild(titleTag);
-    document.getElementById(i).appendChild(descriptionTag);
-    document.getElementById(i).appendChild(removeButton);
 
-    document.getElementById(i).appendChild(editButton);
+        let checkbox = document.createElement("input");
+        checkbox.setAttribute("type", "checkbox");
+
+        let titleTag = document.createElement("h2");
+        let titleContent = document.createTextNode(notes[i].title);
+        titleTag.appendChild(titleContent);
+            
+        let descriptionTag = document.createElement("p");
+        let descriptionContent = document.createTextNode(notes[i].description);
+        descriptionTag.appendChild(descriptionContent);
+    
+        let removeButton = document.createElement("button");
+        removeButton.setAttribute("class", "removeNote icon-bin");
+        removeButton.setAttribute("onclick", "removeNote(this.parentElement.id)");
+            
+        let editButton = document.createElement("button");
+        editButton.setAttribute("class", "editButton icon-edit");
+        editButton.setAttribute("onclick", "editNote(this.parentElement.id)");
+
+        document.getElementById(i).appendChild(checkbox);
+        document.getElementById(i).appendChild(titleTag);
+        document.getElementById(i).appendChild(descriptionTag);
+        document.getElementById(i).appendChild(removeButton);
+    
+        document.getElementById(i).appendChild(editButton);
+    }
+
 }
 
 function removeNote(parentId) {
@@ -66,25 +90,17 @@ function updateNoteId() {
 
 function editNote(parentId) {
 
-    let titleEdit = document.createElement("input");
-    titleEdit.setAttribute("type", "text");
-    titleEdit.setAttribute("class", "titleEdit");
-    titleEdit.setAttribute("id", "newTitle")
-    titleEdit.setAttribute("value", notes[parentId].title);
-        
-    let descriptionEdit = document.createElement("input");
-    descriptionEdit.setAttribute("type", "text");
-    descriptionEdit.setAttribute("class", "descriptionEdit");
-    descriptionEdit.setAttribute("id", "newDescription");
-    descriptionEdit.setAttribute("value", notes[parentId].description); 
+    let siblings = document.getElementById(parentId).childNodes;
+    console.log(siblings);
+
+    siblings[1].contentEditable = true;
+    siblings[2].contentEditable = true;
 
     let saveButton = document.createElement("button");
     saveButton.setAttribute("class", "saveChanges icon-save");
     saveButton.setAttribute("id", "saveButton-" + parentId);
     saveButton.setAttribute("onclick", "saveNote(this.id)");
-
-    document.getElementById(parentId).appendChild(titleEdit);
-    document.getElementById(parentId).appendChild(descriptionEdit);    
+  
     document.getElementById(parentId).appendChild(saveButton);    
 }
 
@@ -93,16 +109,11 @@ function saveNote(id) {
     
     let siblings = document.getElementById(i).childNodes;
  
-    let newTitle = document.getElementById("newTitle").value;
-    notes[i].title = newTitle;
+    notes[i].title = siblings[1].innerHTML;
+    notes[i].description = siblings[2].innerHTML;
 
-    let newDescription = document.getElementById("newDescription").value;
-    notes[i].description = newDescription;
-
-    siblings[0].innerHTML = notes[i].title;
-    siblings[1].innerHTML = notes[i].description;
-    document.getElementById("newTitle").remove();
-    document.getElementById("newDescription").remove();
+    siblings[1].contentEditable = false;
+    siblings[2].contentEditable = false;
     document.getElementById("saveButton-" + i).remove();
 
 }
