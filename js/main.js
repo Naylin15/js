6808e5bbfb2bdfb70885a0ba64dd8feb168177e8
Naylin Medina <mejiasnam@gmail.com>
2020-04-19 16:55:35 -0400

    refactor: remove description add function to create HTML elements
    Signed-off-by: Naylin Medina <mejiasnam@gmail.com>

--- js/main.js
+++ js/main.js
@@ -6,11 +6,9 @@ form.addEventListener('submit', addNote);
 function addNote(e) {  
     e.preventDefault()  
     let noteTitle = document.getElementById('noteTitle').value;
-    let noteDescription = document.getElementById('noteDescription').value;
 
     notes.push({
         title: noteTitle,
-        description: noteDescription
     })
     document.getElementById('form-add').reset();
     checkNote();
@@ -29,53 +27,105 @@ function checkNote() {
 }
 
 function displayNotes() {
-
     let noteContainer = document.createElement("div");
     noteContainer.setAttribute("class", "noteContainer grid-container");
     noteContainer.setAttribute("id", "noteContainer")
     document.getElementById('notes').appendChild(noteContainer);
 
     for (i=0; i < notes.length; i++){
-
-        let noteArticle = document.createElement("article");
-        noteArticle.setAttribute("class", "note grid-x grid-margin-x");
-        noteArticle.setAttribute("id", i)
+        
+        let noteArticle = createHTML({
+            type: "article",
+            attributes: [
+                {
+                    type: "class",
+                    value: "note grid-x grid-margin-x"
+                },
+                {
+                    type: "id",
+                    value: i
+                }
+            ]
+        });
         document.getElementById('noteContainer').appendChild(noteArticle);
         
-        let checkbox = document.createElement("input");
-        checkbox.setAttribute("type", "checkbox");
-        checkbox.setAttribute("class", " checkbox small-2")
-        
-        let titleTag = document.createElement("h2");
-        let titleContent = document.createTextNode(notes[i].title);
-        titleTag.setAttribute("class", "small-2 medium-4 cell");
-        titleTag.appendChild(titleContent);
-        
-        let descriptionTag = document.createElement("p");
-        let descriptionContent = document.createTextNode(notes[i].description);
-        descriptionTag.setAttribute("class", "small-2 medium-4 cell");
-        descriptionTag.appendChild(descriptionContent);
-    
-        let removeButton = document.createElement("button");
-        removeButton.setAttribute("class", "removeNote icon-bin alert button");
-        removeButton.setAttribute("onclick", "removeNote(this.parentElement.id)");
-            
-        let editButton = document.createElement("button");
-        editButton.setAttribute("class", "editButton icon-edit button");
-        editButton.setAttribute("onclick", "editNote(this.parentElement.id)");
-
+        let checkbox = createHTML({
+            type: "input",
+            attributes: [
+                {
+                    type: "type",
+                    value: "checkbox"
+                },
+                {
+                    type: "class",
+                    value: "checkbox small-2"
+                }
+            ]
+        });
         document.getElementById(i).appendChild(checkbox);
+        
+        let titleTag = createHTML({
+            type: "h2",
+            attributes: [
+                {
+                    type: "class",
+                    value: "small-2 medium-4 cell"
+                }
+            ],
+            description: notes[i].title
+        });
         document.getElementById(i).appendChild(titleTag);
-        document.getElementById(i).appendChild(descriptionTag);
+        
+        let removeButton = createHTML({
+            type: "button",
+            attributes: [
+                {
+                    type: "class",
+                    value: "removeNote icon-bin alert button"
+                },
+                {
+                    type: "onclick",
+                    value: "removeNote(this.parentElement.id)"
+                }
+            ]
+        });
         document.getElementById(i).appendChild(removeButton);
-    
+        
+        let editButton = createHTML({
+            type: "button",
+            attributes: [
+                {
+                    type: "class",
+                    value: "editButton icon-edit button"
+                },
+                {
+                    type: "onclick",
+                    value: "editNote(this.parentElement.id)"
+                }
+            ]
+        });
         document.getElementById(i).appendChild(editButton);
     }
 
 }
 
+function createHTML(content) {
+    let newElement = document.createElement(content.type);
+    if (content.attributes) {
+        content.attributes.forEach(attribute => {
+            newElement.setAttribute(attribute.type, attribute.value);
+            
+        });
+    }
+    if (content.description) {
+        let descriptionContent = document.createTextNode(content.description);
+        newElement.appendChild(descriptionContent);
+
+    }
+    return newElement;
+}
+
 function removeNote(parentId) {
-    
     document.getElementById(parentId).remove();
 
     notes.splice(parentId, 1);
@@ -83,55 +133,86 @@ function removeNote(parentId) {
 }
 
 function editNote(parentId) {
-
     let saveButton = document.getElementById("saveButton-" + parentId);
 
     if(saveButton == null) {
         let siblings = document.getElementById(parentId).childNodes;
         siblings[1].contentEditable = true;
-        siblings[2].contentEditable = true;
     
-        let saveButton = document.createElement("button");
-        saveButton.setAttribute("class", "saveChanges icon-save success button");
-        saveButton.setAttribute("id", "saveButton-" + parentId);
-        saveButton.setAttribute("onclick", "saveNote(this.id)");
-      
+        let saveButton = createHTML({
+            type: "button",
+            attributes: [
+                {
+                    type: "class",
+                    value: "saveChanges icon-save success button"
+                },
+                {
+                    type: "id",
+                    value: "saveButton-" + parentId
+                },
+                {
+                    type: "onclick",
+                    value: "saveNote(this.id)"
+                }
+            ]
+        });
         document.getElementById(parentId).appendChild(saveButton);    
+      
     } else {
         warningMessage(parentId);
-    
     }
-
 }
 
 function warningMessage(parentId) {
-
-    let warning = document.createElement("div");
-    warning.setAttribute("class", "alertMessage callout warning small-12");
-    warning.setAttribute("data-closable", "")
-
-    let header = document.createElement("h5");
-    header.innerHTML = "Warning";
-
-    let paragraph = document.createElement("p");
-    paragraph.innerHTML = "The note is alredy being edited";
-    
-    let closeButton = document.createElement("button");
-    closeButton.setAttribute("class", "close-button");
-    closeButton.setAttribute("type", "button");
-    closeButton.setAttribute("data-close", "");
-    closeButton.innerHTML = "x";
-
+    let warning = createHTML({
+        type: "div",
+        attributes: [
+            {
+                type: "class",
+                value: "alertMessage callout warning small-12"
+            },
+            {
+                type: "data-closable"
+            }
+        ]
+    });
     document.getElementById(parentId).appendChild(warning);
-    warning.appendChild(header);
-    warning.appendChild(paragraph);
+
+    let warningHeader = createHTML({
+        type: "h5",
+        description: "Warning"
+    });
+    warning.appendChild(warningHeader);
+
+    let warningContent = createHTML({
+        type: "p",
+        description: "The note is alredy being edited"
+    });
+    warning.appendChild(warningContent);
+
+    let closeButton = createHTML({
+        type: "button",
+        attributes: [
+            {
+                type: "class",
+                value: "close-button"
+            },
+            {
+                type: "type",
+                value: "button"
+            },
+            {
+                type: "data-close"
+            }
+        ],
+        description: "x"
+    });
     warning.appendChild(closeButton);
 
     setTimeout(function() {
         $(".alertMessage").fadeOut("3000", warning.remove());
     }, 3000
     );
-    
 }
 
 function saveNote(id) {
@@ -140,10 +221,7 @@ function saveNote(id) {
     let siblings = document.getElementById(i).childNodes;
  
     notes[i].title = siblings[1].innerHTML;
-    notes[i].description = siblings[2].innerHTML;
 
     siblings[1].removeAttribute("contentEditable");
-    siblings[2].removeAttribute("contentEditable");
     document.getElementById("saveButton-" + i).remove();
-
 }
