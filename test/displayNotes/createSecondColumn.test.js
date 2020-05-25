a6e34861df5c3d3e5a37a02b6051373cab383f93
Naylin Medina <mejiasnam@gmail.com>
2020-05-24 20:48:43 -0400

    test: remove dropdown function
    Signed-off-by: Naylin Medina <mejiasnam@gmail.com>

--- test/displayNotes/createSecondColumn.test.js
+++ test/displayNotes/createSecondColumn.test.js
@@ -1,42 +1,41 @@
-require('../../semantic/out/components/dropdown');
-const $ = require('jquery');
-// jest.dontMock('jquery');
-// jest.dontMock('../../semantic/out/components/dropdown');
 import createSecondColumn from '../../src/displayNotes/createSecondColumn';
 
 test('create second column', () => {
     document.body.innerHTML =
     '<div id="0">' +
     '</div>';
-    
 
     let secondColumn = createSecondColumn(0);
     const divContainer = document.getElementById('0');
     divContainer.appendChild(secondColumn);
-    $('.ui.dropdown').dropdown();
-    
 
     expect(divContainer.innerHTML).toBe(
-        '<div class="column">' +
+        '<div class="column">'+
             '<div class="ui two column center aligned grid">' +
                 '<div class="center aligned column">' +
-                    '<div class="ui dropdown" tabindex="0">' +
-                    '<i class="tag icon"></i>'+
-                    '<div class="menu" tabindex="-1">' +
-                    '<div class="scrolling menu">' +
-                    '<div class="item">' +
-                    '<div class="ui red empty circular label"></div><span>Important</span></div>' +
-                    '<div class="item">' +
-                    '<div class="ui blue empty circular label"></div><span>Later</span></div>' +
+                    '<div class="ui dropdown labeled icon button" id="noteCategory-0">' +
+                    '<i class="tag icon"></i>' +
+                    '<div class="menu">' +
+                        '<div class="scrolling menu">' +
+                            '<div class="item" data-value="important">' +
+                                '<div class="ui red empty circular label"></div>' +
+                                '<span>Important</span></div>' +
+                            '<div class="item" data-value="tomorrow">' +
+                                '<div class="ui yellow empty circular label"></div>' +
+                                '<span>Tomorrow</span></div>' +
+                            '<div class="item" data-value="later">' +
+                                '<div class="ui blue empty circular label"></div>' +
+                                '<span>Later</span></div>' +
+                        '</div>'+
                     '</div>' +
+                    '<span class="text">Category</span>' +
                     '</div>' +
-                    '</div>' +
-                    '<div class="ui dropdown" tabindex="0">' +
+                    '<div class="ui dropdown">' +
                     '<i class="ellipsis vertical icon"></i>' +
-                    '<div class="menu" tabindex="-1">' +
-                    '<div class="scrolling menu">' +
-                    '<div class="item" id="removeNote-0">Delete</div>' +
-                    '<div class="item" id="editNote-0">Edit</div>' +
+                    '<div class="menu">' +
+                        '<div class="scrolling menu">' +
+                        '<div class="item" id="removeNote-0">Delete</div>' +
+                        '<div class="item" id="editNote-0">Edit</div>' +
                     '</div>' +
                     '</div>' +
                     '</div>' +
