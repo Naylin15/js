139bc47e870d8441278d5aa76ba1d95867e6a944
Naylin Medina <mejiasnam@gmail.com>
2020-05-30 22:21:18 -0400

    test: fix addNote
    Signed-off-by: Naylin Medina <mejiasnam@gmail.com>

--- test/addNote.test.js
+++ test/addNote.test.js
@@ -1,19 +1,23 @@
-jest.mock('../src/index');
 import { addNote } from '../src/addNote';
+import * as index from '../src/index';
+import * as check from '../src/checkNote';
+jest.mock('../src/checkNote');
+
 test('add note', () => {
+    jest.spyOn(index.notes, 'push');
     document.body.innerHTML =
-    `<form id="add-form">
+    `<form id="form-add">
     <input type="text" name="noteTitle" id="noteTitle" value="abc">
     <button type="submit" id="button">Add</button>
     </form>
     <div id="notes">
     </div>`;
-    
-    let notes = [];
 
+    const form = document.getElementById('form-add');
+    form.addEventListener('submit', addNote);
     const addButton = document.getElementById('button');
     addButton.click();
 
-
-    expect(notes[0].title).toBe('abc');
+    expect(index.notes.push).toHaveBeenCalledWith({title: 'abc'});
+    expect(check.checkNote).toHaveBeenCalled();
 });
