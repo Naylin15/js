6b0d73cdb4afc1c602152547da782385081198ba
Naylin Medina <mejiasnam@gmail.com>
2020-06-26 22:16:13 -0400

    feat: add functions for notes array
    Signed-off-by: Naylin Medina <mejiasnam@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,5 +1,35 @@
 import { addNote } from './addNote';
 
 export var notes = [];
-const form = document.getElementById('form-add');
-form.addEventListener('submit', addNote);
+document.addEventListener('DOMContentLoaded', function () {
+    $('.ui.dropdown').dropdown();
+    const form = document.getElementById('form-add');
+    form.addEventListener('submit', addNote);
+});
+
+export function changeTitle(i, title) {
+    let newArray = notes;
+    newArray[i].title = title;
+    return newArray;
+}
+
+export function addNoteToArray(noteTitle) {
+    let newArray = notes;
+    newArray.push({
+        title: noteTitle,
+    });
+    return newArray;
+}
+
+export function removeObjectNote(i) {
+    let newArray = notes;
+    newArray.splice(i, 1);
+    return newArray;
+}
+
+
+// function displayByCategories(categoryName) {
+//     var categoryNotes = notes.filter(function(notes){
+//         return notes.category === categoryName;
+//     });
+// }
