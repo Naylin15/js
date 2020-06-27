6fba0a91f250de8c7f9d76e372d31857178a58ac
Naylin Medina <mejiasnam@gmail.com>
2020-06-26 21:17:23 -0400

    feat: change to pure function
    Signed-off-by: Naylin Medina <mejiasnam@gmail.com>

--- src/addNote.js
+++ src/addNote.js
@@ -1,14 +1,14 @@
-import { notes } from './index';
 import { checkNote } from './checkNote';
+import { displayNotes } from './displayNotes';
+import { addNoteToArray } from './index';
 
 export function addNote(e) {
     e.preventDefault();
     let noteTitle = document.getElementById('noteTitle').value;
 
-    notes.push({
-        title: noteTitle,
-    });
+    let notesArray = addNoteToArray(noteTitle);
 
     document.getElementById('form-add').reset();
     checkNote();
+    displayNotes(notesArray);
 }
