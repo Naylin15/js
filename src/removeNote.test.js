dbd326c8d40a5e04cd63e4f36a98859d7e446f55
Naylin Medina <mejiasnam@gmail.com>
2020-07-02 22:41:41 -0400

    test: fix saveNote
    Signed-off-by: Naylin Medina <mejiasnam@gmail.com>

--- src/removeNote.test.js
+++ src/removeNote.test.js
@@ -8,9 +8,7 @@ test('remove note', () => {
 
     document.body.innerHTML =
     '<div id="notes"><article class="article ui two column stackable grid" id="0"><div class="item" id="removeNote-0">Delete</div></article></div>';
-    const removeNoteEvent = document.getElementById('removeNote-0');
-    removeNoteEvent.addEventListener('click', function(){removeNote(0);});
-    removeNoteEvent.click();
+    removeNote(0);
 
     let removedNote = document.getElementById('0');
 
--- src/saveNote.test.js
+++ src/saveNote.test.js
@@ -1,22 +1,20 @@
 import { saveNote } from '../src/saveNote';
-import { notes } from '../src/index';
-import * as index from '../src/index';
-import * as check from '../src/checkNote';
-jest.mock('../src/checkNote');
+import { displayNotes } from '../src/displayNotes/index';
+import { checkNote } from '../src/checkNote';
 jest.mock('../src/index');
+jest.mock('../src/displayNotes/index');
+jest.mock('../src/checkNote');
 
 test('save note', () => {
-
-    index.notes[0] = [{ title: 'abc' }];
 
     document.body.innerHTML ='<div id="notes"><article id="0"><div><div><div><input type="text" id="editInput-0" value="abcd"/><button class="ui positive icon button" id="saveButton-0"></button></div></div></div></article></div>';
 
     saveNote(document.getElementById('saveButton-0').parentElement);
-    const editInput = document.getElementById('editInput-0');
     const saveButton = document.getElementById('saveButton-0');
-    
+    const editInput = document.getElementById('editInput-0');
+
     expect(editInput).toBe(null);
     expect(saveButton).toBe(null);
-    expect(notes[0].title).toBe('abcd');
-    expect(check.checkNote).toHaveBeenCalled();
+    expect(displayNotes).toHaveBeenCalled();
+    expect(checkNote).toHaveBeenCalled();
 });
